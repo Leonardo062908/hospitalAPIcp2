d757819df8d57a398973d2c16fe59a3a50970705
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-04-06 13:43:07 -0300

    Inicializando projeto api de um hospital com spring em JAVa

---
+++ src/test/java/br/com/fiap/hospitalAPI/HospitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hospitalAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
