6cc127d3e127634d2548ea61c581528a05a96318
Gabriel Marques de Lima Sousa <gabriel.sousa@brasoftware.com.br>
2025-04-07 20:53:38 -0300

    Atualizando especialidade dto

--- src/main/java/br/com/fiap/hospitalAPI/mapper/EspecialidadeMapper.java
+++ src/main/java/br/com/fiap/hospitalAPI/mapper/EspecialidadeMapper.java
@@ -1,39 +1,24 @@
 package br.com.fiap.hospitalAPI.mapper;
 
-import br.com.fiap.hospitalAPI.dto.EspecialidadeDTO;
-import br.com.fiap.hospitalAPI.model.Doutor;
+import br.com.fiap.hospitalAPI.dto.EspecialidadeRequestDTO;
+import br.com.fiap.hospitalAPI.dto.EspecialidadeResponseDTO;
 import br.com.fiap.hospitalAPI.model.Especialidade;
-
-import java.util.stream.Collectors;
 
 public class EspecialidadeMapper {
 
-    public static EspecialidadeDTO toDTO(Especialidade especialidade) {
-        EspecialidadeDTO dto = new EspecialidadeDTO();
+    public static Especialidade toEntity(EspecialidadeRequestDTO dto) {
+        Especialidade especialidade = new Especialidade();
+        especialidade.setNome(dto.getNome());
+        especialidade.setDescricao(dto.getDescricao());
+        especialidade.setDoutores(null); // Relacionamentos tratados no service
+        return especialidade;
+    }
+
+    public static EspecialidadeResponseDTO toResponseDTO(Especialidade especialidade) {
+        EspecialidadeResponseDTO dto = new EspecialidadeResponseDTO();
         dto.setId(especialidade.getId());
         dto.setNome(especialidade.getNome());
         dto.setDescricao(especialidade.getDescricao());
-
-        if (especialidade.getDoutores() != null) {
-            dto.setDoutorIds(
-                    especialidade.getDoutores().stream()
-                            .map(Doutor::getId)
-                            .collect(Collectors.toList())
-            );
-        }
-
         return dto;
-    }
-
-    public static Especialidade toEntity(EspecialidadeDTO dto) {
-        Especialidade especialidade = new Especialidade();
-        especialidade.setId(dto.getId());
-        especialidade.setNome(dto.getNome());
-        especialidade.setDescricao(dto.getDescricao());
-
-        // Relacionamentos devem ser tratados no service
-        especialidade.setDoutores(null);
-
-        return especialidade;
     }
 }
