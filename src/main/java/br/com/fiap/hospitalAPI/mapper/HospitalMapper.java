1127a405af27c2ddd0da234435019dc290d373e5
Gabriel Marques de Lima Sousa <gabriel.sousa@brasoftware.com.br>
2025-04-08 16:05:05 -0300

    Desenvolvendo HospitalMapper

---
+++ src/main/java/br/com/fiap/hospitalAPI/mapper/HospitalMapper.java
@@ -0,0 +1,27 @@
+package br.com.fiap.hospitalAPI.mapper;
+
+import br.com.fiap.hospitalAPI.dto.HospitalRequestDTO;
+import br.com.fiap.hospitalAPI.dto.HospitalResponseDTO;
+import br.com.fiap.hospitalAPI.model.Hospital;
+
+public class HospitalMapper {
+
+    public static Hospital toEntity(HospitalRequestDTO dto) {
+        Hospital hospital = new Hospital();
+        hospital.setNome(dto.getNome());
+        hospital.setCnpj(dto.getCnpj());
+        hospital.setEndereco(EnderecoMapper.toEntity(dto.getEndereco()));
+        hospital.setDoutores(null);  // tratamos em outra lógica
+        hospital.setPacientes(null); // tratamos em outra lógica
+        return hospital;
+    }
+
+    public static HospitalResponseDTO toResponseDTO(Hospital hospital) {
+        HospitalResponseDTO dto = new HospitalResponseDTO();
+        dto.setId(hospital.getId());
+        dto.setNome(hospital.getNome());
+        dto.setCnpj(hospital.getCnpj());
+        dto.setEndereco(EnderecoMapper.toResponseDTO(hospital.getEndereco()));
+        return dto;
+    }
+}
