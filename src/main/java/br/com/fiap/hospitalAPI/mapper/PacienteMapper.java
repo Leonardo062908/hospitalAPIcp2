9a5b486d24139be98615e189d419130f7985e83f
Gabriel Marques de Lima Sousa <gabriel.sousa@brasoftware.com.br>
2025-04-08 16:08:17 -0300

    Desenvolvendo Paciente Mapper

---
+++ src/main/java/br/com/fiap/hospitalAPI/mapper/PacienteMapper.java
@@ -0,0 +1,44 @@
+package br.com.fiap.hospitalAPI.mapper;
+
+import br.com.fiap.hospitalAPI.dto.PacienteRequestDTO;
+import br.com.fiap.hospitalAPI.dto.PacienteResponseDTO;
+import br.com.fiap.hospitalAPI.model.Doutor;
+import br.com.fiap.hospitalAPI.model.Paciente;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PacienteMapper {
+
+    public static Paciente toEntity(PacienteRequestDTO dto) {
+        Paciente paciente = new Paciente();
+        paciente.setNome(dto.getNome());
+        paciente.setCpf(dto.getCpf());
+        paciente.setDataNascimento(dto.getDataNascimento());
+        paciente.setDoencas(dto.getDoencas());
+        paciente.setTelefone(dto.getTelefone());
+        paciente.setHospital(null); // setado no service
+        paciente.setDoutores(null); // setado no service
+        return paciente;
+    }
+
+    public static PacienteResponseDTO toResponseDTO(Paciente paciente) {
+        PacienteResponseDTO dto = new PacienteResponseDTO();
+        dto.setId(paciente.getId());
+        dto.setNome(paciente.getNome());
+        dto.setCpf(paciente.getCpf());
+        dto.setDataNascimento(paciente.getDataNascimento());
+        dto.setDoencas(paciente.getDoencas());
+        dto.setTelefone(paciente.getTelefone());
+
+        dto.setHospitalId(paciente.getHospital() != null ? paciente.getHospital().getId() : null);
+
+        List<Long> doutorIds = paciente.getDoutores() != null
+                ? paciente.getDoutores().stream().map(Doutor::getId).collect(Collectors.toList())
+                : Collections.emptyList();
+
+        dto.setDoutorIds(doutorIds);
+        return dto;
+    }
+}
