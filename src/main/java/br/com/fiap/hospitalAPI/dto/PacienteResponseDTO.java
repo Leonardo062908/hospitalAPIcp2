5097e447c0df8c467a9c3b379dacb346edff8345
Gabriel Marques de Lima Sousa <gabriel.sousa@brasoftware.com.br>
2025-04-08 16:08:45 -0300

    Desenvolvendo Paciente response DTO

---
+++ src/main/java/br/com/fiap/hospitalAPI/dto/PacienteResponseDTO.java
@@ -0,0 +1,81 @@
+package br.com.fiap.hospitalAPI.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class PacienteResponseDTO {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String doencas;
+    private String telefone;
+    private Long hospitalId;
+    private List<Long> doutorIds;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getDoencas() {
+        return doencas;
+    }
+
+    public void setDoencas(String doencas) {
+        this.doencas = doencas;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Long getHospitalId() {
+        return hospitalId;
+    }
+
+    public void setHospitalId(Long hospitalId) {
+        this.hospitalId = hospitalId;
+    }
+
+    public List<Long> getDoutorIds() {
+        return doutorIds;
+    }
+
+    public void setDoutorIds(List<Long> doutorIds) {
+        this.doutorIds = doutorIds;
+    }
+}
