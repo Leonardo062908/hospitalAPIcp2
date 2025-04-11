45ebda7c48dbf55ababa7c322011f4d50e94c12b
Ribaas <ribasleonardo92@gmail.com>
2025-04-11 15:11:37 -0300

    chore: adicionando swagger

--- src/main/java/br/com/fiap/hospitalAPI/HospitalApiApplication.java
+++ src/main/java/br/com/fiap/hospitalAPI/HospitalApiApplication.java
@@ -1,9 +1,13 @@
 package br.com.fiap.hospitalAPI;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "API de Hospital", description = "API desenvolvida para o CP2 da matéria Java Advanced - Turma 2TDSPW - Prof Luiz - FIAP", version = "v1"))
 public class HospitalApiApplication {
 
 	public static void main(String[] args) {
